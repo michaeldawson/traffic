c7f709237ce834d4af4a81c94c7dbc3e05f40227
Michael <m.dawson@y7mail.com>
2014-10-19 12:34:29 +1030

    Precompiling assets

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -11,7 +11,7 @@ class Notification < ActiveRecord::Base
     sender = SmsGlobal::Sender.new :user => '92lrp5ps', password: 'd3YwQQVz'
 
     text = "Traffic issue in #{self.issue.suburb}: #{self.issue.description}. \n"
-    text = text + "By the way, heard of EasyFuel? Every dollar you spend gets you 'cents off' at the pump - 10, 20, 50c/L off and more!"
+    text = text + "By the way, heard of EasyFuel? Every dollar you spend gets you 'cents off' at the pump - 10, 20, 30c/L off and more!"
 
     sender.send_text text, self.user.mobile, '0435129029'
   end
