231d65ec290f1cc04dbf5c098678932bba5df5bf
Michael <m.dawson@y7mail.com>
2014-10-19 11:10:57 +1030

    Welcome page work

--- app/assets/javascripts/app/static.js
+++ app/assets/javascripts/app/static.js
@@ -14,8 +14,29 @@ APP.static = {
     $('#route-add').click(function(){
       var to = $('#route-to').val();
       var from = $('#route-from').val();
+      var start_time = $('#route-starttime').val();
+      var end_time = $('#route-endtime').val();
 
       Maps.drawRoute(from, to);
+
+      data = {
+        route: {
+          route_from: from,
+          route_to: to,
+          start_time: start_time,
+          end_time: end_time
+        }
+      }
+
+      // $.post({
+      //   '/routes',
+      //   data,
+      //   success: function() {}
+      // })
+
+      $.post( "routes", data, function( data ) {
+        $( ".result" ).html( data );
+      });
 
       $('#route-to').val('');
       $('#route-from').val('');
